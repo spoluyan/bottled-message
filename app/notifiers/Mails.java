579d06b3b58024b55a40f63628d3ca488b59c7d7
Sergey Poluyan <spoluyan@exadel.com>
2015-06-01 16:03:56 +0300

    Mail notifications

--- app/notifiers/Mails.java
+++ app/notifiers/Mails.java
@@ -3,19 +3,31 @@ package notifiers;
 import play.mvc.Mailer;
 
 public class Mails extends Mailer {
-    public static void sendBottle(String senderName, String senderEmail, String recipientEmail, String link) {
-        // TODO
+    public static void sendBottle(String senderName, String senderEmail, String recipientEmail, String uuid) {
+        setSubject("%s sent you a message in a botlle!", senderName);
+        addRecipient(recipientEmail);
+        setFrom(senderName + " <" + senderEmail + ">");
+        send(senderName, uuid);
     }
 
     public static void sendNotificationBottleIsOpened(String senderEmail, String recipientEmail) {
-        // TODO
+        setSubject("Your bottle was opened!");
+        addRecipient(senderEmail);
+        setFrom(recipientEmail);
+        send(recipientEmail);
     }
 
-    public static void sendBottleBack(String senderEmail, String recipientEmail, String link) {
-        // TODO
+    public static void sendBottleBack(String senderEmail, String recipientEmail, String uuid) {
+        setSubject("Your bottle was sent back");
+        addRecipient(senderEmail);
+        setFrom(recipientEmail);
+        send(recipientEmail, uuid);
     }
 
     public static void sendNotificationBottleIsDiscarded(String senderEmail, String recipientEmail) {
-        // TODO
+        setSubject("Your bottle was lost...");
+        addRecipient(senderEmail);
+        setFrom(recipientEmail);
+        send(recipientEmail);
     }
 }
